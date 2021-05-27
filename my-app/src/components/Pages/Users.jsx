f8a26a7eb6baebc10e209f27885f2d8096ae8e0f
andyska <andreaskamelka@gmail.com>
2021-05-27 18:38:47 -0300

    incio trabajo de crud de user

---
+++ my-app/src/components/Pages/Users.jsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from 'react'
+import {Table , Button} from 'antd'
+import axios from 'axios'
+import {DeleteOutlined , EditOutlined , PlusCircleOutlined} from '@ant-design/icons';
+
+
+const UsersCrud = () => {
+
+  const [users, setUsers] = useState([])
+  const [isModalVisible, setIsModalVisible] = useState(true);
+
+  const handleOnClick =() => {
+    setIsModalVisible(true)
+    console.log('visible:', isModalVisible)
+  }
+  
+  const getAllUsers = async () => {
+    const resp = await axios.get(`localhost:8080/api/users`);
+    console.log(resp.data)
+    setUsers(resp.data)
+  }
+
+  useEffect(() =>{
+    getAllUsers()
+  },[]
+  )
+
+  const [usermodal, setModal] = useState(false)
+  const openModal = ()=>{
+      setModal(true)
+   }
+   const [ userdetails, setUsersdetails]  = useState({})
+
+  const handleOnDelete = (event) => {
+    //antes de borrar llamar a un modal que confirme que quiere borrar ese libro
+    setUsersdetails (event)
+   // console.log('books-handleOnDelete', bookdetails)
+    setIsModalVisible(true)
+    } 
+
+  const columns = [
+    {
+      title: 'Nombre',
+      dataIndex: 'firstName',
+      key: 'firstName',
+    },
+    {
+      title: 'Apellido',
+      dataIndex: 'lastName',
+      key: 'lastName',
+    },
+    {
+      title: 'Usuario',
+      dataIndex: 'userName',
+      key: 'userName',
+    },
+    {
+      title: 'Password',
+      dataIndex: 'password',
+      key: 'password',
+    },
+    {
+      title: 'Mail',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Tipo',
+      dataIndex: 'type',
+      key: 'type',
+    },
+    {
+      title: 'Actions',
+      dataIndex: 'actions',
+      key: 'actions',
+      render: (text, row) =>
+        <>
+          <DeleteOutlined style={{fontSize:'25px', color:'red'}}  onClick={()=>handleOnDelete(row)}/>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <EditOutlined style={{fontSize:'25px', color:'blue'}} />
+        </>
+    },
+  ];
+  
+  
+  return (
+    <div>
+      <h1>Administracion de Usuarios</h1>
+      <Button type="primary" icon={<PlusCircleOutlined/>} onClick={ openModal} >Agregar Usuario</Button>
+      <Table dataSource={users} columns={columns} rowKey="_id"/>
+    </div>
+
+  )
+}
+
+export default UsersCrud
