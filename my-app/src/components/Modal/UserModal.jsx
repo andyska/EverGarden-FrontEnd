cf3f3d6b1d83a2a400ad094fabc978f9259c1306
andyska <andreaskamelka@gmail.com>
2021-05-29 00:38:24 -0300

    agrego mensajes y completo POST y DELETE

--- my-app/src/components/Modal/UserModal.jsx
+++ my-app/src/components/Modal/UserModal.jsx
@@ -1,39 +1,54 @@
 import React, {  useState } from 'react'
-import { Modal , Button, Form , Input} from 'antd'
+import { Modal , Button, Form , Input, message, Select} from 'antd'
 import axios from 'axios'
 
 const {Item}=Form
+const { Option } = Select;
 
 const UserModal =({usermodal, setModal , getAllUsers}) =>{
-    console.log('dentro del user modal- modal', usermodal)
+    const [form] = Form.useForm();
+    
+    const [newtype,setNewType] =useState('')
+    const handletype = t =>{
+       // console.log('habndletype t',t )
+        setNewType(t);
+    }
+
     const [newuser, setNewuser] = useState({
         firstName: '',
         lastName: '',
-        userName: '',
+        userName: 'X',
         password:'',
-        email:  '' ,
-        type: ''
+        email:  '' 
     } )
+
     
     const closeModal = ()=>{
         setModal(false)
     }
+    
     const handleNewUser=e=>{
+       // console.log('target- e',e , 'name' ,name )
         const {name, value } = e.target ;
         setNewuser({...newuser,
             [name]: value});
-           // console.log('handlenewuser',newuser)
-        }
+          //  console.log('handlenewuser-name', name, '-',newuser)
+    }
+
     const saveModal = async ()=>{
+        setNewuser({...newuser, type:newtype})
+        //console.log(form.validateFields)
         try{
-           console.log('FRONT-save modal-newuser:', newuser)
+           //console.log('FRONT-save modal-newuser:', newuser)
             const response = await axios.post('http://localhost:8080/api/users/' , newuser )
             //validar que salio ok el post para refrescar la tabla
-            console.log('Front-new USER -response',response)
+            //console.log('Front-new USER -response',response)
+            message.success("Se creo usuario: " + response.data.userName)
             closeModal()
             getAllUsers()
         } catch (error) {
-            console.log("POST newuser Failed", error)
+            //console.log("POST newuser Failed", error)
+            message.error("Fallo la Grabacion del usuario - Error:"  + error)
             throw error
         }
     }
@@ -45,7 +60,7 @@ const UserModal =({usermodal, setModal , getAllUsers}) =>{
             span:16
         },
         }
-           
+      
 return (
     <div>
       <Modal title='Datos del Usuario' 
@@ -56,23 +71,27 @@ return (
             <Button type="primary" onClick={saveModal}>Guardar</Button>
         ]}>
          <Form {...formview}>
-             <Item label="Nombre">
-                 <Input name="firstName" onChange={handleNewUser}/>
+             <Item label="Nombre" name="firstName" rules={[{ required: true, message: 'Por favor ingrese el NOMBRE' }]}>
+                 <Input name="firstName" onChange={handleNewUser} allowClear/>
              </Item>
-             <Item label="Apellido">
-                 <Input name="lastName" onChange={handleNewUser}/>
+             <Item label="Apellido" name="lastName" rules={[{ required: true, message: 'Por favor ingrese el APELLIDO' }]}>
+                 <Input name="lastName" onChange={handleNewUser} allowClear/>
              </Item>
-             <Item label="Usuario">
-                 <Input name="userName" onChange={handleNewUser}/>
+             <Item label="Tipo" name="type" rules={[{ required: true, message: 'Seleccione el TIPO de usuario' }]}>
+                <Select width="80%"
+                    placeholder="Seleccione Tipo de Usuario"
+                    onChange={handletype}
+                    allowClear
+                    >
+                    <Option value="admin">admin</Option>
+                    <Option value="visita">visita</Option>
+                </Select>
              </Item>
-             <Item label="Tipo">
-                 <Input name="type" onChange={handleNewUser}/>
+             <Item label="Mail" name="email" rules={[{ required: true, message: 'Por favor ingrese el Mail' }]}>
+                 <Input name="email" onChange={handleNewUser} allowClear/>
              </Item>
-             <Item label="Mail">
-                 <Input name="email" onChange={handleNewUser}/>
-             </Item>
-             <Item label="Password">
-                 <Input name="password" onChange={handleNewUser}/>
+             <Item label="Password"  name="password" rules={[{ required: true, message: 'Ingrese PASSWORD (max. 10 Caracteres)' }]}>
+                 <Input.Password name="password" onChange={handleNewUser} allowClear/>
              </Item>
          </Form>
       </Modal>
